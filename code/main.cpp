c0ef22f6bb29132846990ae912816fa2b665abf6
Abdelaziz Zakareya <17045@stemegypt.edu.eg>
2021-02-19 19:45:29 +0200

    Docker

---
+++ code/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "helper.cpp"
+
+using namespace std;
+
+bool solveNQ() 
+{ 
+    int board[N][N] = { { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 } }; 
+  
+    if (solveNQUtil(board, 0) == false) { 
+        printf("Solution does not exist"); 
+        return false; 
+    } 
+  
+    printSolution(board); 
+    return true; 
+} 
+  
+int main() 
+{ 
+    solveNQ(); 
+    return 0; 
+} 
